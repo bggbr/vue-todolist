8a214a765312bea27fe3555baaa05e371b3c7e27
wjdqkdwo@naver.com <jaejung603@gmail.com>
2021-03-24 23:34:48 +0900

    input & button logic

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>input</div>
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      v-model="newTodoItem"
+      placeholder="Type what you have to do"
+      v-on:keyup.enter="addTodo"
+    />
+    <span v-on:click="addTodo">
+      <i class="fas fa-plus"></i>
+    </span>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    };
+  },
+  methods: {
+    addTodo: function() {
+      if (this.newTodoItem !== "") {
+        let value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    }
+  }
+};
 </script>
 <style></style>
